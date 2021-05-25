f4eb13ee499bb83b3e0c8030f5164c0723e9247e
Ilya Kharlamov <ilia3546@me.com>
2021-05-25 19:25:36 +0300

    SPM Support

--- Package.swift
+++ Package.swift
@@ -1,22 +1,27 @@
+// swift-tools-version:5.3.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "Graphene",
-    platforms: [
-        .iOS(.v12)
-    ],
+    platforms: [.iOS(.v12), .macOS(.v10_13)],
     products: [
         .library(name: "Graphene", targets: ["Graphene"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.2")
+        .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: "5.2.0"))
     ],
     targets: [
-        .target(name: "Fraphene",
-                dependencies: ["Alamofire"],
-                path: "Source")
-    ],
-    swiftLanguageVersions: [
-        .v5
+        .target(
+            name: "Graphene",
+            dependencies: ["Alamofire"],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "GrapheneTests",
+            dependencies: ["Graphene"],
+            path: "Tests"
+        )
     ]
 )
