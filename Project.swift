8b75ec0accac6250c02d46117e31934b07d1ac7d
Ilya Kharlamov <ilia3546@me.com>
2021-07-20 11:57:30 +0300

    Fix errors

--- Project.swift
+++ Project.swift
@@ -3,11 +3,13 @@ import ProjectDescriptionHelpers
 
 let project = Library(
     name: .Graphene,
-    options: .allowAppExtentionAPIOnly,
     dependencies: TargetDependencies(
         thirdParty: [.Alamofire]
     ),
     test: TestTarget(
         name: .GrapheneTests
-    )
+    ),
+    additionalSettings: [
+        .allowAppExtentionAPIOnly(true)
+    ]
 ).project
