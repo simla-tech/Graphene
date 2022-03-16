21b5ebebdd8a47ad27a7d5882674d05fe10ae0a9
Ilya Kharlamov <ilia3546@me.com>
2022-03-16 19:30:30 +0700

    Fix GraphQLErrors release compiling

--- Sources/Models/GraphQLErrors.swift
+++ Sources/Models/GraphQLErrors.swift
@@ -7,40 +7,7 @@
 
 import Foundation
 
-public protocol ArrayError: Collection, LocalizedError, CustomNSError {
-    associatedtype ErrorType: CustomNSError
-    var errors: [ErrorType] { get }
-}
-
-extension ArrayError {
-
-    // The upper and lower bounds of the collection, used in iterations
-    public var startIndex: Int { return self.errors.startIndex }
-    public var endIndex: Int { return self.errors.endIndex }
-
-    // Required subscript, based on a dictionary index
-    public subscript(index: Int) -> ErrorType {
-        return self.errors[index]
-    }
-
-    // Method that returns the next index when iterating
-    public func index(after i: Int) -> Int {
-        return self.errors.index(after: i)
-    }
-
-    public static var errorDomain: String { "ArrayError.\(ErrorType.errorDomain)" }
-
-    public var errorUserInfo: [String: Any] {
-        return ["errors": self.errors.map({ $0.errorUserInfo })]
-    }
-
-    public var errorDescription: String? {
-        return self.errors.map(\.localizedDescription).joined(separator: ", ")
-    }
-
-}
-
-public struct GraphQLErrors: ArrayError, Decodable {
+public struct GraphQLErrors: Error, Decodable {
 
     public let errors: [GraphQLError]
 
@@ -48,4 +15,37 @@ public struct GraphQLErrors: ArrayError, Decodable {
         self.errors = errors
     }
 
+}
+
+extension GraphQLErrors: Collection {
+    
+   public var startIndex: Int { return self.errors.startIndex }
+   public var endIndex: Int { return self.errors.endIndex }
+
+   public subscript(index: Int) -> GraphQLError {
+       return self.errors[index]
+   }
+
+   public func index(after i: Int) -> Int {
+       return self.errors.index(after: i)
+   }
+    
+}
+
+extension GraphQLErrors: CustomNSError {
+    
+    public static var errorDomain: String { "GraphQLErrors" }
+
+    public var errorUserInfo: [String: Any] {
+        return ["errors": self.errors.map({ $0.errorUserInfo })]
+    }
+    
+}
+
+extension GraphQLErrors: LocalizedError {
+    
+    public var errorDescription: String? {
+        return self.errors.map(\.localizedDescription).joined(separator: ", ")
+    }
+    
 }
