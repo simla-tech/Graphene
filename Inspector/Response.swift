06615cca71ed07a8097dc53329c4f639cb402f73
Ilya Kharlamov <ilia3546@me.com>
2022-07-20 18:30:49 +0300

    Fix lint errors

--- Inspector/Response.swift
+++ Inspector/Response.swift
@@ -9,23 +9,23 @@ import Foundation
 import Graphene
 
 public extension GrapheneInspector {
-    
+
     struct Response: Decodable {
-        
+
         public let errors: [GraphQLError]
         public let deprecated: [GraphQLError]
-        
+
         private enum CodingKeys: String, CodingKey {
             case errors
             case deprecated
         }
-        
+
         public init(from decoder: Decoder) throws {
             let container = try decoder.container(keyedBy: CodingKeys.self)
             self.errors = (try? container.decodeIfPresent([GraphQLError].self, forKey: .errors)) ?? []
             self.deprecated = (try? container.decodeIfPresent([GraphQLError].self, forKey: .deprecated)) ?? []
         }
-        
+
     }
-    
+
 }
